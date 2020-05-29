2df5e8cee031e53bcf99f303bc5bf11acdd09313
Ankith09 <62370207+Ankith09@users.noreply.github.com>
2020-05-29 21:45:51 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -34,6 +34,7 @@ This is a Telegram Rename Bot!
 Support Group : @InFoTelGroup
 © @super_botsz
 Any issues contact me:- @ankith_m
+
     REPLY_TO_DOC_FOR_RENAME_FILE = "Reply to a Telegram media to `/rename New Name.extension` with custom thumbnail support.."
     ABUSIVE_USERS = "You are not allowed to use this bot. If you think this is a mistake, please check /me to remove this restriction."
     FREE_USER_LIMIT_Q_SZE = """Cannot Process.
