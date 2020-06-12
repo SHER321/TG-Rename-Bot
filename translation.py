48931ed14ca7c6f7831e94f09759f62a7b7f1318
SHER321 <66766275+SHER321@users.noreply.github.com>
2020-06-12 17:39:36 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,12 +1,12 @@
 class Translation(object):
     START_TEXT = """Hello,
-This is a Telegram URL Upload Bot! Created BY @Nithinofficial_admin
+This is a Telegram URL Upload Bot! Created BY @NGYNY
 
 <b>Please send me any direct download URL Link, i can upload to telegram as File/Video 💯</b>
 
 /help for more details..
 
-Support Group : @NM_Projects , @NithinMovies
+Support Group : @MOVIERG_NY
 """
     RENAME_403_ERR = "Sorry. You are not permitted to rename this file."
     ABS_TEXT = " Please don't be selfish."
@@ -19,9 +19,9 @@ URL | filename | username | password"""
     UPLOAD_START = "📤 Trying To Upload📤"
     RCHD_BOT_API_LIMIT = "size greater than maximum allowed size (50MB). Neverthless, trying to upload."
     RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry. But, I cannot upload files greater than 1.5GB due to Telegram API limitations."
-    AFTER_SUCCESSFUL_UPLOAD_MSG = "Please rate me if you find me useful. Join : @NM_Projects"
-    AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds. \nJoin : @NM_Projects \nUploaded in {} seconds."
-    AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds. \nJoin : @NM_Projects \nUploaded in {} seconds."
+    AFTER_SUCCESSFUL_UPLOAD_MSG = "Please rate me if you find me useful. Join : @ALL_MOVIES_LIABRARY"
+    AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds. \nJoin : @ALL_MOVIES_LIABRARY \nUploaded in {} seconds."
+    AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds. \nJoin : @ALL_MOVIES_LIABRARY \nUploaded in {} seconds."
     NOT_AUTH_USER_TEXT = "Please /upgrade your subscription."
     NOT_AUTH_USER_TEXT_FILE_SIZE = "Detected File Size: {}. Free Users can only upload: {}\nPlease /upgrade your subscription.\nIf you think this is a bug, please contact <a href='https://telegram.dog/ThankTelegram'>@SpEcHlDe</a>"
     SAVED_CUSTOM_THUMB_NAIL = "Custom video / file thumbnail saved. This image will be used in the video / file."
@@ -52,8 +52,8 @@ Expires on: When You are in Heaven"""
 --------
 Send /me to know current plan details
 
-Support Group : @NM_Projects ,
-@NithinMovies"""
+Support Group : @MOVIERG_NY ,
+Support Channel : @ALL_MOVIES_LIABRARY"""
     REPLY_TO_DOC_GET_LINK = "Reply to a Telegram media to get High Speed Direct Download Link"
     REPLY_TO_DOC_FOR_C2V = "Reply to a Telegram media to convert"
     REPLY_TO_DOC_FOR_SCSS = "Reply to a Telegram media to get screenshots"
